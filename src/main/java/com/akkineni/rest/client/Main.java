2d62e1c868befae844bd6a65317a46fc6507d98b
akkinenivijay <akkineni.vijay@gmail.com>
2011-06-02 21:25:36 -0400

    Changed the Main Java File.
    Signed-off-by: akkinenivijay <akkineni.vijay@gmail.com>

--- src/main/java/com/akkineni/rest/client/Main.java
+++ src/main/java/com/akkineni/rest/client/Main.java
@@ -1,36 +1,52 @@
 package com.akkineni.rest.client;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+
+import com.sun.jersey.api.container.grizzly2.GrizzlyWebContainerFactory;
+
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		try {
-			final String baseUri = "http://localhost:9998/";
-			final Map<String, String> initParams = new HashMap<String, String>();
-			initParams.put("com.sun.jersey.config.property.packages",
-					"com.akkineni.jersey");
-			System.out.println("Starting grizzly...");
-			// SelectorThread threadSelector;
-			// threadSelector = GrizzlyWebContainerFactory.create(baseUri,
-			// initParams);
-			System.out
-					.println(String
-							.format("Jersey app started with WADL available at %sapplication.wadl\n” + “Try out %shelloworld\nHit enter to stop it...",
-									baseUri, baseUri));
-			System.in.read();
-			// threadSelector.stopEndpoint();
-
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+	private static int getPort(int defaultPort) {
+		String port = System.getProperty("jersey.test.port");
+		if (null != port) {
+			try {
+				return Integer.parseInt(port);
+			} catch (NumberFormatException e) {
+			}
 		}
-		System.exit(0);
+		return defaultPort;
+	}
+
+	private static URI getBaseURI() {
+		return UriBuilder.fromUri("http://localhost/").port(getPort(9998))
+				.build();
+	}
+
+	public static final URI BASE_URI = getBaseURI();
+
+	protected static HttpServer startServer() throws IOException {
+		final Map<String, String> initParams = new HashMap<String, String>();
+		initParams.put("com.sun.jersey.config.property.packages",
+				"com.akkineni.rest.resource");
+		System.out.println("Starting grizzly...");
+		return GrizzlyWebContainerFactory.create(BASE_URI, initParams);
+	}
+
+	public static void main(String[] args) throws IOException {
+		HttpServer httpServer = startServer();
+		System.out
+				.println(String
+						.format("Jersey app started with WADL available at "
+								+ "%sapplication.wadl\nTry out %scustomers\\echo \nHit enter to stop it...",
+								BASE_URI, BASE_URI));
+		System.in.read();
+		httpServer.stop();
 	}
 }
