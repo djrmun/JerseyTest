86eecda45faa14d661c862452ad3da8bfb98052c
akkinenivijay <akkineni.vijay@gmail.com>
2011-06-22 07:04:37 -0400

    Changed the response type with jackson
    Signed-off-by: akkinenivijay <akkineni.vijay@gmail.com>

--- src/main/java/com/akkineni/cometd/service/EchoService.java
+++ src/main/java/com/akkineni/cometd/service/EchoService.java
@@ -3,13 +3,22 @@
  */
 package com.akkineni.cometd.service;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.cometd.bayeux.Message;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.cometd.bayeux.server.BayeuxServer;
+import org.cometd.bayeux.server.ServerMessage;
 import org.cometd.bayeux.server.ServerSession;
 import org.cometd.server.AbstractService;
+
+import com.akkineni.rest.util.DateTimeUtil;
+import com.akkineni.schema.so.ServiceOrderDTO;
+import com.akkineni.schema.so.ServiceOrderSearch;
+import com.sun.research.ws.wadl.Response;
 
 /**
  * @author lokesh
@@ -26,14 +35,51 @@ public class EchoService extends AbstractService {
 		addService("/service/echo", "processEcho");
 	}
 
-	public void processEcho(ServerSession remote, Message message) {
+	public void processEcho(ServerSession remote, ServerMessage.Mutable message) {
 		Map<String, Object> input = message.getDataAsMap();
 		String name = (String) input.get("name");
 
 		Map<String, Object> output = new HashMap<String, Object>();
 		output.put("greeting", "Hello, " + name);
-		remote.deliver(getServerSession(), "/echo", output, null);
-		// getBayeux().getChannel(channel).publish(getServerSession(), message);
+
+		System.out.println(output + " " + message);
+		message.setData(output);
+
+		DateTimeUtil util = new DateTimeUtil();
+
+		ServiceOrderSearch sos = new ServiceOrderSearch();
+
+		for (int i = 0; i < 10; i++) {
+			ServiceOrderDTO dto = new ServiceOrderDTO();
+			dto.setCreationDate(util.getDate());
+			dto.setDescription("Service Order Description");
+			dto.setFlowableID(i);
+			dto.setOrderNumber("1234" + i);
+			dto.setVersion(1);
+
+			sos.getServiceOrderDTO().add(dto);
+		}
+
+		String response = "";
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			response = mapper.writeValueAsString(sos);
+			System.out.println(response);
+		} catch (JsonGenerationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// remote.deliver(getServerSession(), message);
+		remote.deliver(getServerSession(), "/echo", response, null);
+		// getBayeux().getChannel(message.getChannel()).publish(
+		// getServerSession(), message);
 	}
 
 }
--- src/main/webapp/script/application.js
+++ src/main/webapp/script/application.js
@@ -1,81 +1,85 @@
-(function($)
-{
-    var cometd = $.cometd;
+(function($) {
+	var cometd = $.cometd;
 
-    $(document).ready(function()
-    {
-        function _connectionEstablished()
-        {
-            $('#body').append('<div>CometD Connection Established</div>');
-        }
+	$(document).ready(
+			function() {
+				function _connectionEstablished() {
+					$('#body').append(
+							'<div>CometD Connection Established</div>');
+				}
 
-        function _connectionBroken()
-        {
-            $('#body').append('<div>CometD Connection Broken</div>');
-        }
+				function _connectionBroken() {
+					$('#body').append('<div>CometD Connection Broken</div>');
+				}
 
-        function _connectionClosed()
-        {
-            $('#body').append('<div>CometD Connection Closed</div>');
-        }
+				function _connectionClosed() {
+					$('#body').append('<div>CometD Connection Closed</div>');
+				}
 
-        // Function that manages the connection status with the Bayeux server
-        var _connected = false;
-        function _metaConnect(message)
-        {
-            if (cometd.isDisconnected())
-            {
-                _connected = false;
-                _connectionClosed();
-                return;
-            }
+				// Function that manages the connection status with the Bayeux
+				// server
+				var _connected = false;
+				function _metaConnect(message) {
+					if (cometd.isDisconnected()) {
+						_connected = false;
+						_connectionClosed();
+						return;
+					}
 
-            var wasConnected = _connected;
-            _connected = message.successful === true;
-            if (!wasConnected && _connected)
-            {
-                _connectionEstablished();
-            }
-            else if (wasConnected && !_connected)
-            {
-                _connectionBroken();
-            }
-        }
+					var wasConnected = _connected;
+					_connected = message.successful === true;
+					if (!wasConnected && _connected) {
+						_connectionEstablished();
+					} else if (wasConnected && !_connected) {
+						_connectionBroken();
+					}
+				}
 
-        // Function invoked when first contacting the server and
-        // when the server has lost the state of this client
-        function _metaHandshake(handshake)
-        {
-            if (handshake.successful === true)
-            {
-                cometd.batch(function()
-                {
-                    cometd.subscribe('/echo', function(message)
-                    {
-                        $('#body').append('<div>Server Says: ' + message.data.greeting + '</div>');
-                    });
-                    // Publish on a service channel since the message is for the server only
-                    var randomnumber=Math.floor(Math.random()*100);
-                    cometd.publish('/service/echo', { name: 'World :'+randomnumber });
-                });
-            }
-        }
+				// Function invoked when first contacting the server and
+				// when the server has lost the state of this client
+				function _metaHandshake(handshake) {
+					if (handshake.successful === true) {
+						cometd.batch(function() {
+							cometd.subscribe('/echo', function(message) {
+								var obj = jQuery.parseJSON(message.data);
+								// $('#body').append('<div>Server Says: ' +
+								// message.data.greeting + '</div>');
+								$.each(obj.serviceOrderDTO, function(i, val) {
+									$('#body').append(
+											'<div>Server Order : '
+													+ val.flowableID + " : "
+													+ val.orderNumber
+													+ '</div>');
+								});
+								// $('#body').append(
+								// '<div>Server Says: '
+								// + obj.serviceOrderDTO
+								//												+ '</div>');
+							});
+							// Publish on a service channel since the message is
+							// for the server only
+							var randomnumber = Math.floor(Math.random() * 100);
+							cometd.publish('/service/echo', {
+								name : 'World :' + randomnumber
+							});
+						});
+					}
+				}
 
-        // Disconnect when the page unloads
-        $(window).unload(function()
-        {
-            cometd.disconnect(true);
-        });
+				// Disconnect when the page unloads
+				$(window).unload(function() {
+					cometd.disconnect(true);
+				});
 
-        var cometURL = "http://localhost:8080/JerseyTest/cometd";
-        cometd.configure({
-            url: cometURL,
-            logLevel: 'debug'
-        });
+				var cometURL = "http://localhost:8080/JerseyTest/cometd";
+				cometd.configure({
+					url : cometURL,
+					logLevel : 'debug'
+				});
 
-        cometd.addListener('/meta/handshake', _metaHandshake);
-        cometd.addListener('/meta/connect', _metaConnect);
+				cometd.addListener('/meta/handshake', _metaHandshake);
+				cometd.addListener('/meta/connect', _metaConnect);
 
-        cometd.handshake();
-    });
+				cometd.handshake();
+			});
 })(jQuery);
