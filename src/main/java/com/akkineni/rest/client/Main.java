3c7b449fa60a2a0699b6d9f9124458eb01ccd753
Vijay Akkineni <akkineni.vijay@gmail.com>
2012-07-08 21:25:06 -0400

    Fixed the Main.java with the new grizzly jars

--- src/main/java/com/akkineni/rest/client/Main.java
+++ src/main/java/com/akkineni/rest/client/Main.java
@@ -2,42 +2,28 @@ package com.akkineni.rest.client;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.ws.rs.core.UriBuilder;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 
-import com.sun.jersey.api.container.grizzly2.servlet.GrizzlyWebContainerFactory;
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
 
 public class Main {
 
-	private static int getPort(int defaultPort) {
-		String port = System.getProperty("jersey.test.port");
-		if (null != port) {
-			try {
-				return Integer.parseInt(port);
-			} catch (NumberFormatException e) {
-			}
-		}
-		return defaultPort;
-	}
-
 	private static URI getBaseURI() {
-		return UriBuilder.fromUri("http://localhost/").port(getPort(8080))
-				.build();
+		return UriBuilder.fromUri("http://localhost/").port(9998).build();
 	}
 
-	public static final URI BASE_URI = UriBuilder.fromUri(
-			getBaseURI().toString() + "JerseyTest/rest/").build();
+	public static final URI BASE_URI = getBaseURI();
 
 	protected static HttpServer startServer() throws IOException {
-		final Map<String, String> initParams = new HashMap<String, String>();
-		initParams.put("com.sun.jersey.config.property.packages",
-				"com.akkineni.rest.resource");
 		System.out.println("Starting grizzly...");
-		return GrizzlyWebContainerFactory.create(BASE_URI, initParams);
+		ResourceConfig rc = new PackagesResourceConfig(
+				"com.akkineni.rest.resource");
+		return GrizzlyServerFactory.createHttpServer(BASE_URI, rc);
 	}
 
 	public static void main(String[] args) throws IOException {
