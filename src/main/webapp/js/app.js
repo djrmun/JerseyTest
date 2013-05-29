09ad8449e51873d504f2fff9d6e2cc80a77466d9
Vijay Akkineni <akkineni.vijay@gmail.com>
2013-05-29 15:31:34 -0400

    added app angularjs

---
+++ src/main/webapp/js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/* App Module */
+
+angular.module('phonecat', ['phonecatFilters', 'phonecatServices']).
+  config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+      when('/phones', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
+      when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
+      when('/permissions', {templateUrl: 'partials/permissions.html',   controller: PermissionController}).
+      otherwise({redirectTo: '/permissions'});
+}]);
---
+++ src/main/webapp/js/controllers.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/* Controllers */
+
+function PhoneListCtrl($scope, Phone) {
+  $scope.phones = Phone.query();
+  $scope.orderProp = 'age';
+}
+
+//PhoneListCtrl.$inject = ['$scope', 'Phone'];
+
+function PhoneDetailCtrl($scope, $routeParams, Phone) {
+  $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+    $scope.mainImageUrl = phone.images[0];
+  });
+
+  $scope.setImage = function(imageUrl) {
+    $scope.mainImageUrl = imageUrl;
+  }
+}
+
+function PermissionController($scope,$http){
+      $scope.url = 'http://localhost:8080/JerseyTest/rest/ldap/permission/all';
+
+      $scope.search = function() {
+        $http.get($scope.url).
+        success(function(data,status){
+          $scope.status = status;
+          $scope.permissions = data;
+
+        }).
+        error(function(data,status){
+          $scope.data = data || "Request failed";
+          $scope.status=status;
+        });
+      };    
+  }
+
+//PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone'];
