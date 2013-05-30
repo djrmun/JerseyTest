08c30bb925e058c0e97b130be35b43180993870e
Vijay Akkineni <akkineni.vijay@gmail.com>
2013-05-30 07:44:00 -0400

    form ready for create permission

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -3,51 +3,70 @@
 /* Controllers */
 
 function PhoneListCtrl($scope, Phone) {
-  $scope.phones = Phone.query();
-  $scope.orderProp = 'age';
+	$scope.phones = Phone.query();
+	$scope.orderProp = 'age';
 }
 
-//PhoneListCtrl.$inject = ['$scope', 'Phone'];
+// PhoneListCtrl.$inject = ['$scope', 'Phone'];
 
 function PhoneDetailCtrl($scope, $routeParams, Phone) {
-  $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-    $scope.mainImageUrl = phone.images[0];
-  });
+	$scope.phone = Phone.get({
+		phoneId : $routeParams.phoneId
+	}, function(phone) {
+		$scope.mainImageUrl = phone.images[0];
+	});
 
-  $scope.setImage = function(imageUrl) {
-    $scope.mainImageUrl = imageUrl;
-  }
+	$scope.setImage = function(imageUrl) {
+		$scope.mainImageUrl = imageUrl;
+	}
 }
 
-function PermissionController($scope,$http){
-      $scope.url = 'http://localhost:8080/JerseyTest/rest/ldap/permission/all';
+function PermissionController($scope, $http) {
+	$scope.url = 'http://localhost:8080/JerseyTest/rest/ldap/permission/all';
 
-      $scope.search = function() {
-        $http.get($scope.url).
-        success(function(data,status){
-          $scope.status = status;
-          $scope.permissions = data;
+	$scope.search = function() {
+		$http.get($scope.url).success(function(data, status) {
+			$scope.status = status;
+			$scope.permissions = data;
 
-        }).
-        error(function(data,status){
-          $scope.data = data || "Request failed";
-          $scope.status=status;
-        });
-      };    
-  }
-
-function CreatePermissionController($scope,$http){
-	 $scope.master= {};
-	 
-	 $scope.update = function(perm) {
-		 $scope.master= angular.copy(perm);
-	 };
-	  
-	 $scope.reset = function() {
-		 $scope.perm = angular.copy($scope.master);
-	 };
-	  
-	 $scope.reset();
+		}).error(function(data, status) {
+			$scope.data = data || "Request failed";
+			$scope.status = status;
+		});
+	};
 }
 
-//PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone'];
+function CreatePermissionController($scope, $http) {
+
+	$scope.domains = [ {
+		name : 'domain_serviceorder'
+	}, {
+		name : 'domain_subscription'
+	}, {
+		name : 'domain_admin'
+	} ];
+
+	$scope.types = [ {
+		name : 'type_permission'
+	}, {
+		name : 'type_nti'
+	} ];
+	
+	$scope.master = {};
+
+	$scope.update = function(perm) {
+		var arr = [];
+		arr.push($scope.selectedDomain);
+		arr.push($scope.selectedType);
+		$scope.perm.cuaPermissionCategory=arr;
+		$scope.master = angular.copy(perm);
+	};
+
+	$scope.reset = function() {
+		$scope.perm = angular.copy($scope.master);
+	};
+
+	$scope.reset();
+}
+
+// PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone'];
