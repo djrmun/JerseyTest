dffc5bdc3f8d2575929f87b61858fcbd20bc8e91
Vijay Akkineni <akkineni.vijay@gmail.com>
2013-05-31 16:06:54 -0400

    removed unnecessary code

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,11 +2,9 @@
 
 /* App Module */
 
-angular.module('phonecat', ['phonecatFilters', 'phonecatServices']).
+angular.module('cuaApp', ['phonecatFilters', 'phonecatServices']).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      when('/phones', {templateUrl: 'partials/phone-list.html',   controller: PhoneListCtrl}).
-      when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: PhoneDetailCtrl}).
       when('/permissions', {templateUrl: 'partials/permissions.html',   controller: PermissionController}).
       when('/profiles', {templateUrl: 'partials/profiles.html',   controller: ProfileController}).
       when('/permcreate', {templateUrl: 'partials/createpermission.html',   controller: CreatePermissionController}).
--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -2,25 +2,6 @@
 
 /* Controllers */
 
-function PhoneListCtrl($scope, Phone) {
-	$scope.phones = Phone.query();
-	$scope.orderProp = 'age';
-}
-
-// PhoneListCtrl.$inject = ['$scope', 'Phone'];
-
-function PhoneDetailCtrl($scope, $routeParams, Phone) {
-	$scope.phone = Phone.get({
-		phoneId : $routeParams.phoneId
-	}, function(phone) {
-		$scope.mainImageUrl = phone.images[0];
-	});
-
-	$scope.setImage = function(imageUrl) {
-		$scope.mainImageUrl = imageUrl;
-	};
-}
-
 function PermissionController($scope, $http) {
 	$scope.url = 'http://localhost:8080/JerseyTest/rest/ldap/permission/all';
 
