08529e864f2cf6959520a7a1e56decdb60b9276d
Vijay Akkineni <akkineni.vijay@gmail.com>
2013-06-10 12:26:01 -0400

    Added normalize css and also added a home menu and home html

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,11 +2,21 @@
 
 /* App Module */
 
-angular.module('cuaApp', ['phonecatFilters', 'phonecatServices']).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/permissions', {templateUrl: 'partials/permissions.html',   controller: PermissionController}).
-      when('/profiles', {templateUrl: 'partials/profiles.html',   controller: ProfileController}).
-      when('/permcreate', {templateUrl: 'partials/createpermission.html',   controller: CreatePermissionController}).
-      otherwise({redirectTo: '/permissions'});
-}]);
+angular.module('cuaApp', [ 'phonecatFilters', 'phonecatServices' ]).config(
+		[ '$routeProvider', function($routeProvider) {
+			$routeProvider.when('/home', {
+				templateUrl : 'partials/home.html',
+				controller : PermissionController
+			}).when('/permissions', {
+				templateUrl : 'partials/permissions.html',
+				controller : PermissionController
+			}).when('/profiles', {
+				templateUrl : 'partials/profiles.html',
+				controller : ProfileController
+			}).when('/permcreate', {
+				templateUrl : 'partials/createpermission.html',
+				controller : CreatePermissionController
+			}).otherwise({
+				redirectTo : '/home'
+			});
+		} ]);
