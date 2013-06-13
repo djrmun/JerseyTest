2fdfdc3341699cac6ea76761f79452845a59529e
Vijay Akkineni <akkineni.vijay@gmail.com>
2013-06-13 10:48:30 -0400

    added a filter to extract domain names from workgroup permissions - Need to fix couple fo permission names to make it consistent.

--- src/main/webapp/js/filters.js
+++ src/main/webapp/js/filters.js
@@ -2,6 +2,32 @@
 
 /* Filters */
 
+Array.prototype.getUnique = function(){
+	var u = {}, unique = [];
+	for (var i = 0; i < this.length; i++) {
+		var domainprefix = this[i].split('.')[0];
+		if(domainprefix=="commontopology"){
+			domainprefix="serviceorder";
+		}
+		if(domainprefix=="alert"){
+			domainprefix="serviceorder";
+		}
+		if(domainprefix=="canned"){
+			domainprefix="reports";
+		}
+		if(domainprefix=="mediation"){
+			domainprefix="reports";
+		}
+		if(u.hasOwnProperty(domainprefix)){
+			continue;
+		}
+		unique.push(domainprefix);
+		u[domainprefix]=1;
+	};
+
+	return unique;
+};
+
 angular.module('phonecatFilters', []).filter('checkmark', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
@@ -14,5 +40,13 @@ angular.module('phonecatFilters', []).filter('checkmark', function() {
 			return input;
 		}
 		
+	};
+}).filter('extractDomainsForPermissions',function(){
+	return function(input) {
+		if(input){
+			return input.getUnique();
+		}else{
+			return input;
+		}
 	};
 });
