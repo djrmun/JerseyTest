6905e58143ac156a9416b04998f5c61dc6e00bcd
Vijay <Akkineni>
2013-06-24 17:09:36 -0400

    fixed perm users report

--- src/main/webapp/app/js/controllers/PermissionUsersController.js
+++ src/main/webapp/app/js/controllers/PermissionUsersController.js
@@ -6,22 +6,17 @@ define(['controllers', 'services'], function (controllers, services) {
     'use strict';
     controllers.controller('PermissionUsersController', ['$scope', '$http', 'PermissionsFactory',
         function ($scope, $http, PermissionsFactory) {
-            $scope.url = '/JerseyTest/rest/ldap/permission/all';
-            function init() {
-                console.log('search invoked');
-                $http
-                    .get($scope.url).success(function (data, status) {
-                        $scope.status = status;
-                        $scope.permissions = data;
-                        $scope.errorMsg = "Successfully fetched all permissions :) " + status;
-                    })
-                    .error(function (data, status) {
-                        $scope.data = data || "Fetching to fetch permissions!!! No Doughnut for you :( ";
-                        $scope.status = status;
-                    });
-            }
+            var promise = PermissionsFactory.get();
+            promise.then(function (result) {
+                console.log(result);
+                $scope.permissions = result.data;
+                $scope.errorMsg = "Successfully fetched all permissions :) " + result.status;
+            }, function (result) {
+                console.log(result);
+                $scope.permissions = [];
+                $scope.errorMsg = "Failed to fetch permissions!!! No Doughnut for you :( ";
+            });
 
-            $scope.permissions = PermissionsFactory.get();
             $scope.update = function () {
                 $http.
                     get('http://localhost:8080/JerseyTest/rest/ldap/user/findUsersWithPermission/' + $scope.selectedPermission.cuaPermissionName)
